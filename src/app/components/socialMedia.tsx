ddb263d5ccda55ab686c86b7abf26488f3981c07
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-05-31 14:12:51 -0300

    add redes sociais

--- src/app/components/socialMedia.tsx
+++ src/app/components/socialMedia.tsx
@@ -5,17 +5,17 @@ export function SocialMedia({ ulStyle = "", liStyle = "" }) {
     return (
         <ul className={ulStyle}>
             <li className={liStyle}>
-                <Link href={""} className="w-full h-full">
+                <Link href={"https://wa.me/5513981788930"} className="w-full h-full">
                     <IoLogoWhatsapp className="w-full h-full" />
                 </Link>
             </li>
             <li className={liStyle}>
-                <Link href={""} className="w-full h-full">
+                <Link href={"https://www.facebook.com/cabeca.cabeca.5851/?locale=pt_BR"} className="w-full h-full">
                     <IoLogoFacebook className="w-full h-full" />
                 </Link>
             </li>
             <li className={liStyle}>
-                <Link href={""} className="w-full h-full">
+                <Link href={"https://instagram.com/cabecas_bar_?igshid=MzRlODBiNWFlZA=="} className="w-full h-full">
                     <IoLogoInstagram className="w-full h-full" />
                 </Link>
             </li>
