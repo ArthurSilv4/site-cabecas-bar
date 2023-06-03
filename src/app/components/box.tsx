3e1b5f5459e313b694c826bb4ec37919652400a7
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-06-03 19:25:29 -0300

    add hover nos linls e mudanca dos amarelos

--- src/app/components/box.tsx
+++ src/app/components/box.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export function Box({ h3Text = "", h2Text = "", pText = "", bntText = "" }) {
     return (
         <div className="bg-bgRed p-8 rounded-md m-auto h-[250px] w-[250px] text-white">
-            <h3 className="text-yellow-400">
+            <h3 className=" text-yellowButton">
                 {h3Text}
             </h3>
             <h2 className="font-bold text-4xl mt-4">
@@ -12,7 +12,7 @@ export function Box({ h3Text = "", h2Text = "", pText = "", bntText = "" }) {
             <p className="font-extrabold text-sm mt-4">
                 {pText}
             </p>
-            <button className="bg-yellow-500 p-2 rounded-xl font-extrabold drop-shadow-lg mt-6">
+            <button className="text-yellowButton p-2 rounded-xl font-extrabold drop-shadow-lg mt-6">
                 <Link href={""} className='h-full w-full'>
                     {bntText}
                 </Link>
--- src/app/components/navegationLinks.tsx
+++ src/app/components/navegationLinks.tsx
@@ -5,22 +5,30 @@ export function NavegationLinks({ ulStyle = "", liStyle = "" }) {
 
         <ul className={ulStyle}>
             <li className={liStyle}>
-                <Link href="#company">
+                <Link href="#company"
+                    className="hover:text-white/75"
+                >
                     Quem somos
                 </Link>
             </li >
             <li className={liStyle}>
-                <Link href="#units">
+                <Link href="#units"
+                    className="hover:text-white/75"
+                >
                     Unidades
                 </Link>
             </li>
             <li className={liStyle}>
-                <Link href="#menu" >
+                <Link href="#menu" 
+                    className="hover:text-white/75"
+                >
                     Cardapio
                 </Link>
             </li>
             <li className={liStyle}>
-                <Link href="#contacts">
+                <Link href="#contacts"
+                    className="hover:text-white/75"
+                >
                     Contatos
                 </Link>
             </li>
