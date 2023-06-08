49a36c2f59812be518e31dfc79760902f0065530
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-06-08 05:53:01 -0300

    update imagem

--- src/app/components/images/imagesSlides.tsx
+++ src/app/components/images/imagesSlides.tsx
@@ -4,6 +4,9 @@ export const homeImage = [
     },
     {
         url: 'https://github.com/ArthurSilv4/site-cabecas-bar/blob/main/public/images/bemVindo.png?raw=true'
+    },
+    {
+        url:'https://github.com/ArthurSilv4/site-cabecas-bar/blob/main/public/images/Delivery.png?raw=true'
     }
 ]
 
