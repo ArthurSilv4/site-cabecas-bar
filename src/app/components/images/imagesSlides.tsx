ae1aafb93ecdc0f8f1012646af9bd77189d2e4dc
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-06-18 19:31:04 -0300

    update url image

--- src/app/components/images/imagesSlides.tsx
+++ src/app/components/images/imagesSlides.tsx
@@ -3,7 +3,7 @@ export const homeImage = [
         url: 'https://github.com/ArthurSilv4/site-cabecas-bar/blob/main/public/images/bemVindo.png?raw=true'
     },
     {
-        url:'https://github.com/ArthurSilv4/site-cabecas-bar/blob/main/public/images/entrega.png?raw=true'
+        url:'https://github.com/ArthurSilv4/site-cabecas-bar/blob/main/public/images/entregaa.png?raw=true'
     }
 ]
 
